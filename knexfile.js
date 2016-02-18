c2fe9981ffd7a7115573c909ae4eaa024bfd6ce9
= <tsmit291@icloud.com>
2016-02-17 20:17:29 -0700

    deploying to heroku

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/restaurants'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true'
+  }
+
+};
